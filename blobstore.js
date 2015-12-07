767bab6b055ba843c2a026d7da58bac096f0ecb7
Jonathan Brown <jbrown@bluedroplet.com>
2015-12-07 16:45:55 +0700

    Use existing web3 config if available. @see https://gist.github.com/frozeman/fbc7465d0b0e6c1c4c23

--- blobstore.js
+++ blobstore.js
@@ -1,6 +1,16 @@
 var Web3 = require('web3');
-var web3 = new Web3();
-web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
+
+// @see https://gist.github.com/frozeman/fbc7465d0b0e6c1c4c23
+if (typeof web3 !== 'undefined') {
+  var defaultAccount = web3.eth.defaultAccount;
+  var web3 = new Web3(web3.currentProvider);
+  web3.eth.defaultAccount = defaultAccount;
+}
+else {
+  var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+  web3.eth.defaultAccount = web3.eth.accounts[0];
+}
+
 var blobstoreAbi = require('./blobstore.abi.json');
 var blobstoreContract = web3.eth.contract(blobstoreAbi);
 var blobstoreAddress = '0x8f3fae469b08e48d8e2a692e0b9805c38509231b';
