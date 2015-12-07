3410f8a2ae54e0b3cc1b7591bed12a1bc3616620
Jonathan Brown <jbrown@bluedroplet.com>
2015-12-07 15:34:26 +0700

    Use updated contract.

--- blobstore.js
+++ blobstore.js
@@ -3,11 +3,10 @@ var web3 = new Web3();
 web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
 var blobstoreAbi = require('./blobstore.abi.json');
 var blobstoreContract = web3.eth.contract(blobstoreAbi);
-var blobstoreAddress = '0x3c531591cb807e01404574076f429d205f5ee981';
+var blobstoreAddress = '0x8f3fae469b08e48d8e2a692e0b9805c38509231b';
 var blobstore = blobstoreContract.at(blobstoreAddress);
 
-// https://github.com/bluedroplet/blobstore-ethereum/blob/c65287e0ff249fec047834e7895cb46c0e090228/blobstore.sol
-// Solidity version: 0.1.7-f86451cd/.-Emscripten/clang/int linked to libethereum-1.1.0-35b67881/.-Emscripten/clang/int
+// solc version: 0.2.0-0/Release-Linux/g++/int linked to libethereum-1.1.0-0/Release-Linux/g++/int
 
 var getBlobHash = function(blob) {
   return '0x' + web3.sha3(blob.toString('hex'), {encoding: 'hex'});
