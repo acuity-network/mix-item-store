5753689724704b2c93f3ee5f94c6854614a21ef1
Jonathan Brown <jbrown@bluedroplet.com>
2015-12-07 14:39:53 +0700

    Add doxygen to contract.

--- blobstore.sol
+++ blobstore.sol
@@ -8,10 +8,20 @@ contract BlobStore {
 
     event logBlob(bytes32 indexed hash, bytes blob) anonymous;
 
+    /**
+     * @dev Gets the block that a blob is logged in.
+     * @param hash sha3 of the blob.
+     * @return Block number.
+     */
     function getBlobBlock(bytes32 hash) constant returns (uint) {
         return blobBlock[hash];
     }
 
+    /**
+     * @dev Stores a blob in the transaction log.
+     * @param blob Blob that should be stored.
+     * @return hash sha3 of the blob.
+     */
     function storeBlob(bytes blob) returns (bytes32 hash) {
         hash = sha3(blob);
         if (blobBlock[hash] == 0) {
