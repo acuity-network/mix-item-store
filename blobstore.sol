e3a82e8a837502a48a1f95088fb72234b1130584
Jonathan Brown <jbrown@bluedroplet.com>
2015-12-11 06:36:35 +0700

    Reverse function ordering.

--- blobstore.sol
+++ blobstore.sol
@@ -9,15 +9,6 @@ contract BlobStore {
     event logBlob(bytes32 indexed hash, bytes blob) anonymous;
 
     /**
-     * @dev Gets the block that a blob is logged in.
-     * @param hash sha3 of the blob.
-     * @return blockNumber Number of the block that the blob is logged in.
-     */
-    function getBlobBlock(bytes32 hash) constant returns (uint blockNumber) {
-        blockNumber = blobBlock[hash];
-    }
-
-    /**
      * @dev Stores a blob in the transaction log.
      * @param blob Blob that should be stored.
      * @return hash sha3 of the blob.
@@ -26,6 +17,15 @@ contract BlobStore {
         bytes32 hash = sha3(blob);
         blobBlock[hash] = block.number;
         logBlob(hash, blob);
+    }
+
+    /**
+     * @dev Gets the block that a blob is logged in.
+     * @param hash sha3 of the blob.
+     * @return blockNumber Number of the block that the blob is logged in.
+     */
+    function getBlobBlock(bytes32 hash) constant returns (uint blockNumber) {
+        blockNumber = blobBlock[hash];
     }
 
 }
