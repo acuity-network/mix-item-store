b772deee4e72d7fd7b81cb33c34e5d5fb5d645d1
Jonathan Brown <jbrown@bluedroplet.com>
2015-12-10 17:31:12 +0700

    Make communication with Ethereum node fully async.

--- blobstore.js
+++ blobstore.js
@@ -22,58 +22,50 @@ var getBlobHash = function(blob) {
   return '0x' + web3.sha3(blob.toString('hex'), {encoding: 'hex'});
 }
 
-var getBlobBlock = function(hash, block) {
+var getBlobBlock = function(hash, block, callback) {
   // Determine the block that includes the transaction for this blob.
-  return blobstore.getBlobBlock(hash, {}, block).toNumber();
+  blobstore.getBlobBlock(hash, {}, block, function(error, result) {
+    if (error) {
+      callback(error);
+    }
+    else {
+      callback(false, result.toNumber());
+    }
+  });
 }
 
-var storeBlob = function(blob) {
+var storeBlob = function(blob, callback) {
   // Determine hash of blob.
   var hash = getBlobHash(blob);
   // Check if this blob already exists 200 blocks back.
-  if (getBlobBlock(hash, web3.eth.blockNumber - 200) == 0) {
-    // Calculate maximum transaction gas.
-    var gas = 44800 + 78 * blob.length;
-    // Broadcast the transaction. If this blob is already in a transaction this
-    // will be handled by the contract.
-    blobstore.storeBlob('0x' + blob.toString('hex'), {gas: gas});
-    // Check that the blob is pending.
-    if (getBlobBlock(hash, 'pending') == 0) {
-      throw "Failed to publish blob.";
+  getBlobBlock(hash, web3.eth.blockNumber - 200, function(error, blobBlock) {
+    if (error) { callback(error); return; }
+    if (blobBlock == 0) {
+      // Calculate maximum transaction gas.
+      var gas = 44800 + 78 * blob.length;
+      // Broadcast the transaction. If this blob is already in a transaction this
+      // will be handled by the contract.
+      blobstore.storeBlob('0x' + blob.toString('hex'), {gas: gas}, function(error, result) {
+        if (error) { callback(error); return; }
+        // Check that the blob is pending.
+        getBlobBlock(hash, 'pending', function(error, blobBlock) {
+          if (blobBlock == 0) {
+            callback("Blob failed to broadcast.");
+          }
+          else {
+            callback(null, true);
+          }
+        });
+      });
     }
-  }
+    else {
+      callback(null, true);
+    }
+  });
   return hash;
 }
 
-var getBlob = function(hash, callback) {
-  var blobBlock = getBlobBlock(hash, 'latest');
-  if (blobBlock == 0) {
-    // The blob isn't in a block yet. See if it is in a pending transaction.
-    // This will only work if the blob was stored directly by a transaction.
-    var txids = web3.eth.getBlock('pending').transactions;
-    for (var i in txids) {
-      var tx = web3.eth.getTransaction(txids[i]);
-      if (tx.to != blobstoreAddress) {
-        continue;
-      }
-      // Extract the blob from the transaction.
-      var length = parseInt(tx.input.substr(74, 64), 16);
-      var blob = new Buffer(tx.input.substr(138, length * 2), 'hex');
-      // Does it have the correct hash?
-      if (getBlobHash(blob) == hash) {
-        callback(null, blob);
-        return;
-      }
-    }
-    // We didn't find the blob. Check in the blocks one more time in case it
-    // just got mined and we missed it.
-    blobBlock = getBlobBlock(hash, 'latest');
-    if (blobBlock == 0) {
-      // We didn't find it. Report the Error.
-      callback('error');
-      return;
-    }
-  }
+function getBlobFromBlock(blobBlock, hash, callback) {
   // If the blob is in a block that occured within the past hour, search from an
   // hour ago until the latest block in case there has been a re-arragement
   // since we got the block number (very conservative).
@@ -87,6 +79,7 @@ var getBlob = function(hash, callback) {
   }
   var filter = web3.eth.filter({fromBlock: fromBlock, toBlock: toBlock, address: blobstoreAddress, topics: [hash]});
   filter.get(function(error, result) {
+    if (error) { callback(error); return; }
     if (result.length != 0) {
       var length = parseInt(result[0].data.substr(66, 64), 16);
       callback(null, new Buffer(result[0].data.substr(130, length * 2), 'hex'));
@@ -95,6 +88,48 @@ var getBlob = function(hash, callback) {
       // There has just been a re-arrangement and the trasaction is now back to
       // pending. Let's try again from the start.
       getBlob(hash, callback);
+    }
+  });
+}
+
+var getBlob = function(hash, callback) {
+  getBlobBlock(hash, 'latest', function(error, blobBlock) {
+    if (error) { callback(error); return; }
+    if (blobBlock == 0) {
+      // The blob isn't in a block yet. See if it is in a pending transaction.
+      // This will only work if the blob was stored directly by a transaction.
+      web3.eth.getBlock('pending', true, function(error, result) {
+        if (error) { callback(error); return; }
+        for (var i in result.transactions) {
+          var tx = result.transactions[i];
+          if (tx.to != blobstoreAddress) {
+            continue;
+          }
+          // Extract the blob from the transaction.
+          var length = parseInt(tx.input.substr(74, 64), 16);
+          var blob = new Buffer(tx.input.substr(138, length * 2), 'hex');
+          // Does it have the correct hash?
+          if (getBlobHash(blob) == hash) {
+            callback(null, blob);
+            return;
+          }
+        }
+        // We didn't find the blob. Check in the blocks one more time in case it
+        // just got mined and we missed it.
+        blobBlock = getBlobBlock(hash, 'latest', function(error, blobBlock) {
+          if (error) { callback(error); return; }
+          if (blobBlock == 0) {
+            // We didn't find it. Report the Error.
+            callback("Blob not found.");
+          }
+          else {
+            getBlobFromBlock(blobBlock, hash, callback);
+          }
+        });
+      });
+    }
+    else {
+      getBlobFromBlock(blobBlock, hash, callback);
     }
   });
 }
