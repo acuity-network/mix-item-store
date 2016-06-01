ab9f0605d947372a6e576b431f88b7feb30121a7
Jonathan Brown <jbrown@bluedroplet.com>
2016-06-02 02:42:43 +0700

    id => hash

--- blobstore.sol
+++ blobstore.sol
@@ -4,18 +4,18 @@
  */
 contract BlobStore {
 
-    event logBlob(bytes32 indexed id, bytes blob) anonymous;
+    event logBlob(bytes32 indexed hash, bytes blob) anonymous;
 
     /**
      * @dev Stores a blob in the transaction log.
      * @param blob Blob that should be stored.
-     * @return id Hash of blob.
+     * @return hash Hash of the blob.
      */
-    function storeBlob(bytes blob) external returns (bytes32 id) {
+    function storeBlob(bytes blob) external returns (bytes32 hash) {
         // Calculate the hash.
-        id = sha3(blob);
+        hash = sha3(blob);
         // Store the blob in a log in the current block.
-        logBlob(id, blob);
+        logBlob(hash, blob);
     }
 
 }
