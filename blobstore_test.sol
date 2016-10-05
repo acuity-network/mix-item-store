a9f20b6c04510a31ba16846c44032c2266f2d604
Jonathan Brown <jbrown@bluedroplet.com>
2016-10-06 01:00:54 +0700

    Add more tests.

--- blobstore_test.sol
+++ blobstore_test.sol
@@ -17,9 +17,57 @@ contract BlobStoreTest is Test {
         blobStore = new BlobStore(blobStoreRegistry);
     }
 
-    function testBlobCreate() {
-        bytes32 id = blobStore.create(hex"001122FF", 0x1234, true, true, true, true, true);
-        assertEq12(bytes12(id), blobStore.getContractId());
+    function testThrowRegisterContractAgain() {
+        blobStoreRegistry.register(blobStore.getContractId());
+    }
+
+    function testThrowBlobStoreNotRegistered() {
+        blobStoreRegistry.getBlobStore(0);
+    }
+
+    function testBlobStoreRegistered() {
+        assertEq(blobStoreRegistry.getBlobStore(blobStore.getContractId()), blobStore);
+    }
+
+    function testThrowCreateExistingNonce() {
+        blobStore.create(hex"00", 0, false, false, false, false, false);
+        blobStore.create(hex"00", 0, false, false, false, false, false);
+    }
+
+    function testCreate() {
+        bytes32 blobId0 = blobStore.create(hex"00", 0, false, false, false, false, false);
+        assertEq12(blobStore.getContractId(), bytes12(blobId0));
+        assertTrue(blobStore.getExists(blobId0));
+        assertEq(blobStore.getOwner(blobId0), this);
+        assertEq(blobStore.getRevisionCount(blobId0), 1);
+        assertFalse(blobStore.getUpdatable(blobId0));
+        assertFalse(blobStore.getEnforceRevisions(blobId0));
+        assertFalse(blobStore.getRetractable(blobId0));
+        assertFalse(blobStore.getTransferable(blobId0));
+
+        bytes32 blobId1 = blobStore.create(hex"00", 1, true, true, true, true, true);
+        assertEq12(blobStore.getContractId(), bytes12(blobId1));
+        assertTrue(blobStore.getExists(blobId1));
+        assertEq(blobStore.getOwner(blobId1), 0);
+        assertEq(blobStore.getRevisionCount(blobId1), 1);
+        assertTrue(blobStore.getUpdatable(blobId1));
+        assertTrue(blobStore.getEnforceRevisions(blobId1));
+        assertTrue(blobStore.getRetractable(blobId1));
+        assertTrue(blobStore.getTransferable(blobId1));
+
+        assertFalse(blobId0 == blobId1);
+    }
+
+    function testThrowsCreateNewRevisionNotUpdatable() {
+        bytes32 blobId = blobStore.create(hex"00", 0, false, false, false, false, false);
+        blobStore.createNewRevision(blobId, hex"00");
+    }
+
+    function testCreateNewRevision() {
+        bytes32 blobId = blobStore.create(hex"00", 0, true, false, false, false, false);
+        uint revisionId = blobStore.createNewRevision(blobId, hex"00");
+        assertEq(revisionId, 1);
+        assertEq(blobStore.getRevisionCount(blobId), 2);
     }
 
 }
