dfe2a0815bd3ac2e0317b6456155d549110d8190
Jonathan Brown <jbrown@bluedroplet.com>
2016-10-16 20:00:18 +0700

    Put all external methods in the abstract contract.

--- abstract_blobstore.sol
+++ abstract_blobstore.sol
@@ -3,8 +3,52 @@ pragma solidity ^0.4.2;
 /**
  * @title AbstractBlobStore
  * @author Jonathan Brown <jbrown@bluedroplet.com>
+ * @dev Contracts must be able to interact with blobs regardless of which BlobStore contract they are stored in, so it is necessary for there to be an abstract contract that defines an interface for BlobStore contracts.
  */
 contract AbstractBlobStore {
+
+    /**
+     * @dev Creates a new blob. It is guaranteed that each user will get a different blobId from the same nonce.
+     * @param contents Contents of the blob to be stored.
+     * @param nonce Any value that the user has not used previously to create a blob.
+     * @param flags Packed blob settings.
+     * @return blobId Id of the blob.
+     */
+    function create(bytes contents, bytes32 nonce, byte flags) external returns (bytes32 blobId);
+
+    /**
+     * @dev Create a new blob revision.
+     * @param blobId Id of the blob.
+     * @param contents Contents of the new revision.
+     * @return revisionId The new revisionId.
+     */
+    function createNewRevision(bytes32 blobId, bytes contents) external returns (uint revisionId);
+
+    /**
+     * @dev Update a blob's latest revision.
+     * @param blobId Id of the blob.
+     * @param contents Contents that should replace the latest revision.
+     */
+    function updateLatestRevision(bytes32 blobId, bytes contents) external;
+
+    /**
+     * @dev Retract a blob's latest revision. Revision 0 cannot be retracted.
+     * @param blobId Id of the blob.
+     */
+    function retractLatestRevision(bytes32 blobId) external;
+
+    /**
+     * @dev Delete all a blob's revisions and replace it with a new blob.
+     * @param blobId Id of the blob.
+     * @param contents Contents that should be stored.
+     */
+    function restart(bytes32 blobId, bytes contents) external;
+
+    /**
+     * @dev Retract a blob.
+     * @param blobId Id of the blob. This blobId can never be used again.
+     */
+    function retract(bytes32 blobId) external;
 
     /**
      * @dev Enable transfer of the blob to the current user.
@@ -19,10 +63,47 @@ contract AbstractBlobStore {
     function transferDisable(bytes32 blobId) external;
 
     /**
-     * @dev Get the id for this BlobStore contract.
-     * @return contractId Id of the contract.
+     * @dev Transfer a blob to a new user.
+     * @param blobId Id of the blob.
+     * @param recipient Address of the user to transfer to blob to.
      */
-    function getContractId() constant external returns (bytes12 contractId);
+    function transfer(bytes32 blobId, address recipient) external;
+
+    /**
+     * @dev Disown a blob.
+     * @param blobId Id of the blob.
+     */
+    function disown(bytes32 blobId) external;
+
+    /**
+     * @dev Set a blob as not updatable.
+     * @param blobId Id of the blob.
+     */
+    function setNotUpdatable(bytes32 blobId) external;
+
+    /**
+     * @dev Set a blob to enforce revisions.
+     * @param blobId Id of the blob.
+     */
+    function setEnforceRevisions(bytes32 blobId) external;
+
+    /**
+     * @dev Set a blob to not be retractable.
+     * @param blobId Id of the blob.
+     */
+    function setNotRetractable(bytes32 blobId) external;
+
+    /**
+     * @dev Set a blob to not be transferable.
+     * @param blobId Id of the blob.
+     */
+    function setNotTransferable(bytes32 blobId) external;
+
+    /**
+     * @dev Get the id for this BlobStore contract.
+     * @return Id of the contract.
+     */
+    function getContractId() constant external returns (bytes12);
 
     /**
      * @dev Check if a blob exists.
@@ -30,6 +111,23 @@ contract AbstractBlobStore {
      * @return exists True if the blob exists.
      */
     function getExists(bytes32 blobId) constant external returns (bool exists);
+
+    /**
+     * @dev Get info about a blob.
+     * @param blobId Id of the blob.
+     * @return flags Packed blob settings.
+     * @return owner Owner of the blob.
+     * @return revisionCount How many revisions the blob has.
+     * @return blockNumbers The block numbers of the revisions.
+     */
+    function getInfo(bytes32 blobId) constant external returns (byte flags, address owner, uint revisionCount, uint[] blockNumbers);
+
+    /**
+     * @dev Get all a blob's flags.
+     * @param blobId Id of the blob.
+     * @return flags Packed blob settings.
+     */
+    function getFlags(bytes32 blobId) constant external returns (byte flags);
 
     /**
      * @dev Determine if a blob is updatable.
@@ -72,5 +170,12 @@ contract AbstractBlobStore {
      * @return revisionCount How many revisions the blob has.
      */
     function getRevisionCount(bytes32 blobId) constant external returns (uint revisionCount);
+
+    /**
+     * @dev Get the block numbers for all of a blob's revisions.
+     * @param blobId Id of the blob.
+     * @return blockNumbers Revision block numbers.
+     */
+    function getAllRevisionBlockNumbers(bytes32 blobId) constant external returns (uint[] blockNumbers);
 
 }
