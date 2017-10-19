51a9852cada21ccd6c9eb07c68c147fe7ed5eb2f
Jonathan Brown <jbrown@mix-blockchain.org>
2017-10-19 17:46:40 +0700

    Fix flag docs.

--- src/item_store_ipfs_sha256.sol
+++ src/item_store_ipfs_sha256.sol
@@ -14,8 +14,8 @@ contract ItemStoreIpfsSha256 is ItemStoreInterface {
     byte constant UPDATABLE = 0x01;           // True if the item is updatable. After creation can only be disabled.
     byte constant ENFORCE_REVISIONS = 0x02;   // True if the item is enforcing revisions. After creation can only be enabled.
     byte constant RETRACTABLE = 0x04;         // True if the item can be retracted. After creation can only be disabled.
-    byte constant TRANSFERABLE = 0x08;        // True if the item be transfered to another user or disowned. After creation can only be disabled.
-    byte constant DISOWN = 0x10;              // True if the item should not have an owner.
+    byte constant TRANSFERABLE = 0x08;        // True if the item can be transfered to another user or disowned. After creation can only be disabled.
+    byte constant DISOWN = 0x10;              // True if the item should not have an owner at creation.
 
     /**
      * @dev Single slot structure of item state.
